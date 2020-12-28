f2e6baddec1de24acabc52c660991132af456c0d
omerjaved65 <omer.oj6544@gmail.com>
2020-12-28 18:33:00 +0500

    remove button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   <div>Omer Javed seekh rhe hy</div>
   <p>today is the 28 of december</p>
   <p>bitcoin</p>
-  <button >Click here</button>
   <br></br>
   <label >React App</label>
   

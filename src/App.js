a4763a3cc054921ad0ea61769ea587286a91feb0
omerjaved65 <omer.oj6544@gmail.com>
2020-12-28 18:28:32 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div>
+  <div>Omer Javed seekh rhe hy</div>
+  <p>today is the 28 of december</p>
+  <p>bitcoin</p>
+  <button >Click here</button>
+  <br></br>
+  <label >React App</label>
+  
+  </div>
+  );
+}
+
+export default App;
